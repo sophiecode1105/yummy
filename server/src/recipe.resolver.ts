5fbcdc23f4b85a1a10a9c9af59163214e172a219
KongJin <pibrod@gmail.com>
2022-04-12 16:04:06 +0900

    [Update] 레시피 (유저-레시피)구현

--- server/src/recipe.resolver.ts
+++ server/src/recipe.resolver.ts
@@ -5,13 +5,26 @@ import { PrismaService } from './prisma.service';
 @Resolver()
 export class RecipeResolver {
   constructor(private prisma: PrismaService) {}
+
   @Query()
   async getAllRecipe(): Promise<Recipe[]> {
     return this.prisma.recipe.findMany({});
   }
 
+  @Query()
+  async getRecipe(@Args('id') id: number): Promise<Recipe> {
+    return this.prisma.recipe.findUnique({
+      where: { id },
+      include: {
+        user: true,
+      },
+    });
+  }
+
   @Mutation()
-  async createRecipe(@Args('info') info: Recipe): Promise<Recipe> {
+  async createRecipe(
+    @Args('info') info: { title: string; userId: number },
+  ): Promise<Recipe> {
     const { title, userId } = info;
     return this.prisma.recipe.create({
       data: {
@@ -24,5 +37,28 @@ export class RecipeResolver {
         user: true,
       },
     });
+  }
+
+  @Mutation()
+  async updateRecipe(
+    @Args('info') info: { id: number; title: string },
+  ): Promise<Recipe> {
+    const { id, title } = info;
+
+    return this.prisma.recipe.update({
+      where: { id },
+      data: { title },
+    });
+  }
+
+  @Mutation()
+  async deleteRecipe(@Args('id') id: number): Promise<Boolean> {
+    try {
+      await this.prisma.recipe.delete({ where: { id } });
+      return true;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
   }
 }
